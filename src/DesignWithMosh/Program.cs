6fa1960e8f3580c98d2f3c29559cf512fd982f90
NICOLAS MALULEKE <nicolusmaluleke@gmail.com>
2021-05-08 03:55:28 +0200

    Initial commit

---
+++ src/DesignWithMosh/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignWithMosh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
