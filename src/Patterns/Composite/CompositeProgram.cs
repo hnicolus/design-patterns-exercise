cd88ae35feb43351d0c1738fb4beba2374ccdde5
hnicolus <nicolusmaluleke@gmail.com>
2021-06-02 16:32:41 +0200

    Changed Target Framework to net5.0 from net6.0

---
+++ src/Patterns/Composite/CompositeProgram.cs
@@ -0,0 +1,24 @@
+namespace Patterns.Composite
+{
+    class CompositeProgram
+    {
+        public static  void Run()
+        {
+                        var group1 = new Group();
+            group1.Add(new Shape("Triangle"));
+            group1.Add(new Shape("Square"));
+
+            var group2 = new Group();
+            group2.Add(new Shape("Circle"));
+            group2.Add(new Shape("Circle"));
+
+            var group3 = new Group();
+
+            group3.Add(group1);
+            group3.Add(group2);
+
+            group3.Render();
+            group3.Move();
+        }
+    }
+}
--- src/Patterns/Program.cs
+++ src/Patterns/Program.cs
@@ -36,30 +36,12 @@ namespace Patterns
             //ChainOfResponsibility();
             //ChainOfResponsibiltyExcercise();
             //VisitorPattern();
-            //CompositePattern();
+            //CompositeProgram.Run();
             // ChatProgram.Run();
             // FireFighterProgram.Run();
             //MediatorPatternChatRoom();
 
             EmailProgram.Run();
-        }
-        private static void CompositePattern()
-        {
-            var group1 = new Group();
-            group1.Add(new Shape("Triangle"));
-            group1.Add(new Shape("Square"));
-
-            var group2 = new Group();
-            group2.Add(new Shape("Circle"));
-            group2.Add(new Shape("Circle"));
-
-            var group3 = new Group();
-
-            group3.Add(group1);
-            group3.Add(group2);
-
-            group3.Render();
-            group3.Move();
         }
 
         private static void VisitorPattern()
